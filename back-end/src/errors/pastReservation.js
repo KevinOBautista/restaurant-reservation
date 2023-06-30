48f4f3b0a7d39ec3d0df4164bd4fed9275459eaf
KevinOBautista <kevinbautista2103@gmail.com>
2023-06-29 23:30:11 -0400

    pastReservation fixed, US-03: open between 10:30am - 9:30pm added

--- back-end/src/errors/pastReservation.js
+++ back-end/src/errors/pastReservation.js
@@ -3,18 +3,29 @@ function asDateString(date) {
 		.toString(10)
 		.padStart(2, "0")}-${date.getDate().toString(10).padStart(2, "0")}`;
 }
-function pastReservation(req, res, next) {
-	const date = req.body.data.reservation_date.split("-");
+function dateValidation(inputDate) {
+	const date = inputDate.split("-");
 	const todays = asDateString(new Date()).split("-");
+
+	if (date[0] < todays[0]) {
+		return true;
+	} else if (date[1] < todays[1]) {
+		return true;
+	} else if (date[1] == todays[1]) {
+		if (date[2] < todays[2]) {
+			return true;
+		}
+	} else {
+		return false;
+	}
+}
+function pastReservation(req, res, next) {
+	const date = req.body.data.reservation_date;
 	const error = new Error(`'reservation_date' must be in the future.`);
 	error.status = 400;
-	const dateTest = new Date();
-	console.log("DateTest: ", dateTest);
-	date.forEach((section, index) => {
-		if (section < todays[index]) {
-			throw error;
-		}
-	});
+	if (dateValidation(date)) {
+		throw error;
+	}
 	next();
 }
 
--- back-end/src/errors/timeValidation.js
+++ back-end/src/errors/timeValidation.js
@@ -11,8 +11,19 @@ function validTime(inputStr) {
 		parseInt(time[1], 10) <= 59
 	);
 }
+function closedTime(inputTime) {
+	const time = inputTime.split(":");
+	if (time[0] < 10 || (time[0] == 10 && time[1] < 30)) {
+		return true;
+	} else if (time[0] > 21 || (time[0] == 21 && time[1] > 30)) {
+		return true;
+	} else {
+		return false;
+	}
+}
 function timeValidation(req, res, next) {
 	const time = req.body.data.reservation_time;
+	const splittedTime = time.split(":");
 	if (!validTime(time)) {
 		const error = new Error(
 			`Property 'reservation_time' must be a valid time.`
@@ -20,7 +31,15 @@ function timeValidation(req, res, next) {
 		error.status = 400;
 		throw error;
 	}
+	if (closedTime(time)) {
+		const error = new Error(
+			`Property 'reservation_time' must be between 10:30AM and 9:30PM.`
+		);
+		error.status = 400;
+		throw error;
+	}
 	next();
 }
 
+console.log(closedTime("21:31"));
 module.exports = timeValidation;
