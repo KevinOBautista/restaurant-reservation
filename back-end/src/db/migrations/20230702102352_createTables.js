0c7bb44baee72ec901d7bbc7c73b2e6993ba121a
Kevin Bautista <kevinbautista2103@gmail.com>
2023-07-10 17:13:24 -0400

    tables fix

--- back-end/src/db/migrations/20230702102352_createTables.js
+++ back-end/src/db/migrations/20230702102352_createTables.js
@@ -1,8 +1,14 @@
 exports.up = function (knex) {
 	return knex.schema.createTable("tables", (table) => {
 		table.increments("table_id").primary();
-		table.string("table_name");
-		table.integer("capacity");
+		table.string("table_name").notNullable();
+		table.integer("capacity").unsigned().notNullable();
+		table
+			.integer("reservation_id")
+			.nullable()
+			.references("reservation_id")
+			.inTable("reservations");
+		table.boolean("occupied").defaultTo(false).notNullable();
 		table.timestamps(true, true);
 	});
 };
